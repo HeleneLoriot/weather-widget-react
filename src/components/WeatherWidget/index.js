c6785ea46f496d612065851fff78ed54393383fd
HeleneLoriot <loriot.helene@gmail.com>
2021-12-06 12:27:55 +0100

    dynamic icon and api key variable in local environnement

--- src/components/WeatherWidget/index.js
+++ src/components/WeatherWidget/index.js
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 // style
 import './style.scss';
@@ -8,10 +9,23 @@ export default function WeatherWidget({ city}) {
   // local state
   const [description, setDescscription] = useState('temps dégagé');
   const [temperature, setTemperature] = useState('13');
+  const [iconId, setIconId] = useState('10d');
+
+  useEffect(() => {
+      // secret key for https://openweathermap.org/ API
+      const apiKey = process.env.REACT_APP_API_KEY;
+
+      //call api open weather
+      console.log('hello')
+  },[city])
+
+  let icon = `http://openweathermap.org/img/wn/${iconId}@2x.png`
 
   return (
     <div className="weather-widget">
-        <div className="weather-widget__icon">&#x1F31E;</div>
+        <div className="weather-widget__iconContainer">
+          <img className="weather-widget__icon"src={icon} alt="weather icon" />
+        </div>
         <div className="weather-widget__infos">
           <p className="weather-widget__city">{city}</p>
           <p className="weather-widget__description">{description}</p>
