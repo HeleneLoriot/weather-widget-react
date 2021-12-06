7ddf4c3f4267a7eb671cafca213d9f44c75e2baf
HeleneLoriot <loriot.helene@gmail.com>
2021-12-06 13:21:57 +0100

    error message if city is not found

--- src/components/WeatherWidget/index.js
+++ src/components/WeatherWidget/index.js
@@ -5,34 +5,39 @@ import axios from 'axios';
 // style
 import './style.scss';
 
-export default function WeatherWidget({ city}) {
+export default function WeatherWidget({ city }) {
   // local state
   const [description, setDescscription] = useState('temps dégagé');
   const [temperature, setTemperature] = useState('13');
   const [iconId, setIconId] = useState('10d');
+  const [error, setError] = useState(false);
 
   useEffect(() => {
       // secret key for https://openweathermap.org/ API
       const apiKey = process.env.REACT_APP_API_KEY;
+
+      setError(false);
 
       //call api open weather
       const loadData = async () => {
         try {
           const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=fr&appId=${apiKey}`);
           const { main: { temp }, weather } = response.data;
-          console.log(response)
-          console.log(response.data)
-          setTemperature(temp);
+          setTemperature(Math.round(temp));
           setIconId(weather[0].icon)
           setDescscription(weather[0].description)
         }
         catch (error) {
-          console.log(error);
+          setTemperature('-');
+          setIconId('11d');
+          setDescscription('');
+          setError(true);
         }
       };
 
     loadData();
   },[city])
+
 
   let icon = `http://openweathermap.org/img/wn/${iconId}@2x.png`
 
@@ -42,7 +47,7 @@ export default function WeatherWidget({ city}) {
           <img className="weather-widget__icon"src={icon} alt="weather icon" />
         </div>
         <div className="weather-widget__infos">
-          <p className="weather-widget__city">{city}</p>
+          {error ? <p className="weather-widget__city">Cette ville n'existe pas</p> : <p className="weather-widget__city">{city}</p> }
           <p className="weather-widget__description">{description}</p>
         </div>
         <div className="weather-widget__temperature">{temperature}<sup>°</sup>C</div>
